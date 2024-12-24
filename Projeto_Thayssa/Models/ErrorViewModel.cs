3d06155cd8f949f506c395f86665909e43c85f2c
Thayssa1509 <gasparthayssa@gmail.com>
2024-12-24 17:00:24 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Thayssa/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Projeto_Thayssa.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
